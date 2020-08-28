c5fc3c80041b20c49fa12601ed75378445b34938
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-08-28 12:23:27 +0530

    final sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -8,4 +8,76 @@ board = [
         [0, 7, 0, 3, 0, 0, 0, 1, 2],
         [1, 2, 0, 0, 0, 7, 4, 0, 0],
         [0, 4, 9, 2, 0, 6, 0, 0, 7]
-    ]
+]
+
+
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - -")
+
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == len(board) - 1:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+
+def find_soln(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10, 1):
+        if is_valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if find_soln(board):
+                return True
+
+            board[row][col] = 0
+    return False
+
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+    return False
+
+
+def is_valid(board, num, pos):
+    # Check row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3, 1):
+        for j in range(box_x * 3, box_x * 3 + 3, 1):
+            if board[i][j] == num and [i, j] != pos:
+                return False
+
+    return True
+
+
+if __name__ == "__main__":
+    print_board(board)
+    print()
+    print("The solution is:")
+    find_soln(board)
+    print_board(board)
